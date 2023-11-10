8ed2d548a4454e84fedd918550b723330deed94c
Dawid <dawidpiecha88@gmail.com>
2023-11-10 20:56:46 +0100

    statistics task finished

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,2 +1,30 @@
 import PropTypes from 'prop-types';
 import css from './Statistics.module.css';
+
+const colorPalette = ['red', 'navy', 'green', 'black', 'purple'];
+
+const Statistics = ({ title, stats }) => (
+  <section className={css.statistics}>
+    <h2 className={css.title}>{title}</h2>
+
+    <ul className={css['stat-list']}>
+      {stats.map((stat, index) => (
+        <li
+          key={stat.id}
+          className={css.item}
+          style={{ backgroundColor: colorPalette[index % colorPalette.length] }}
+        >
+          <span className={css.label}>{stat.label}</span>
+          <span className={css.percentage}>{stat.percentage}%</span>
+        </li>
+      ))}
+    </ul>
+  </section>
+);
+
+Statistics.propTypes = {
+  title: PropTypes.string.isRequired,
+  stats: PropTypes.array.isRequired,
+};
+
+export { Statistics };
